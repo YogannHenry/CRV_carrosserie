a97f561c0cbb7f7278c7b08e900cc4131b29d29e
Yogann <yogann.henry@perfusion.dev>
2023-11-29 10:13:13 +0100

    wop

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center pt-20 justify-center text-2xl">
+    <h1>Page de contact ou ce que vous voulez ^^</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="flex items-center pt-20 justify-center text-2xl"> start coding your web page </p>
+</template>
+
+<script setup></script>

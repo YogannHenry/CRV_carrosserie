48fe24223633179b067f9f0b400e0c58d9223000
Yogann <yogann.henry@perfusion.dev>
2023-11-29 10:13:13 +0100

    wop

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="flex items-center pt-20 justify-center text-2xl">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Contact</RouterLink>
+        
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
